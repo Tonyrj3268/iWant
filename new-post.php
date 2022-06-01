8df635912fb5b341861eb394772c28db2764e87d
TonyLin <Tony910319@gmail.com>
2022-06-01 20:27:28 +0800

    new post page recover

--- new-post.php
+++ new-post.php
@@ -9,20 +9,11 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/jquery-ui.css">
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="./js/app.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js" integrity="sha512-AIOTidJAcHBH2G/oZv9viEGXRqDNmfdPVPYOYKGy3fti0xIplnlgMHUGfuNRzC6FkzIo0iIxgFnr9RikFxK+sw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://kit.fontawesome.com/0999d15b7c.js" crossorigin="anonymous"></script>
-=======
     <link rel="stylesheet" href="./css/jquery.datetimepicker.css">
     <script src="./js/jquery-3.6.0.js"></script>
     <script src="./js/new-post.js"></script>
     <script src="./js/jquery-ui.js"></script>
     <script src="./js/jquery.datetimepicker.full.js"></script>
->>>>>>> 17a6524857b812e010b84f2c440360e9498bc338
 </head>
 <body>
 
@@ -43,10 +34,6 @@
             <a href="index.php" onclick="delCookie('name'),delCookie('user_incession')">確認</a>
             <a onclick="$('.logoutframe').dialog('close')" style="cursor: pointer;">取消</a>
         </div>
-<<<<<<< HEAD
-=======
-        <!-- </form> -->
->>>>>>> 4a0b6faa1f0ab5bf8b0a143129613367bf41123d
     </div>
     <?php include 'php/header_bar.php';?>
         <form action="" method="post" id='post-content' enctype="multipart/form-data">
@@ -61,15 +48,10 @@
                 <input id='img'type="file" name="img" onchange="readURL(this)" targetID="preview_img">
                 <img src="./uploads/default-image.jpg" alt="" id="preview_img">
             </button> 
-<<<<<<< HEAD
             <input type="text" name="price" id="price" placeholder="價格/1小時">
             <input type="text" name="place" id="place" placeholder="面交地點">
             <p>結束日期:<input type="text" class="accept_date" id="accept_date" placeholder="請選擇日期"></p><br>
             <p>可接受之時間:<input type="text" id="accept_time" placeholder="請選擇時間"></p><br>
-=======
-            <input type="text" name="price" id="price" placeholder="價格">
-            <input type="text" name="place" id="place" placeholder="面交地點">
->>>>>>> 4a0b6faa1f0ab5bf8b0a143129613367bf41123d
             <a onclick="appendItem()" style="cursor: pointer;">發文</a>
             <a href='index.php' style="cursor: pointer;">取消</a>
         </form>
